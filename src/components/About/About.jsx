3407f532bcc02be896fe459caa7a8562b1a3de34
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-01-07 12:54:27 +0600

    new file added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div>
+          <h1>This is about page</h1>  
+          <h1>About Us</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+         <p><small>thank you for visited our website</small></p>   
+        </div>
+    );
+};
+
+export default Footer;
